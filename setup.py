22e5bf4a279cb6168d1fdfbfa7a421c8b97f9a48
Marcus Legendre <marcus.legendre@gmail.com>
2016-08-30 19:56:46 +0200

    Added Python 3.3 to setup.py and .travis.yml

--- setup.py
+++ setup.py
@@ -41,6 +41,7 @@ setup(
         'License :: OSI Approved :: MIT License',
 
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
     ],
